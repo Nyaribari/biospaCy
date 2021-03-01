a49ac67fe68498caafe3e07f7ba310b9007f5287
Nyaribari Reuben <39657169+Nyaribari@users.noreply.github.com>
2021-03-01 09:57:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 name='biospacy',
 version='0.0.2',
 description = 'The package is a text labeling function.',
-url = 'git+ssh://git@github.com/Nyaribari/biospaCy#egg=labels',
+url = 'git+ssh://git@github.com/Nyaribari/biospaCy#egg=bios',
 author='Nyarbari Reuben',
 author_email='anyaribari@gmail.com',
 license='unlicense',
