727cb25005d23d1f3d4728a4b8c68e32329b2c99
nyaribari <anyaribari@gmail.com>
2022-03-16 07:27:42 +0300

    fos: author updates

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
 name='biospacy',
-version='0.0.2',
+version='0.0.3',
 description = 'The package is a text labeling function.',
-url = 'git+ssh://git@github.com/Nyaribari/biospaCy#egg=bios',
+url = 'git+ssh://git@github.com/foscraft/biospaCy#egg=bios',
 author='Nyarbari Reuben',
 author_email='anyaribari@gmail.com',
 license='unlicense',
